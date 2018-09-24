9f9cc46e0c7296cf1ea54d33d3346f456b2c8a7f
Diego de la Hera <delahera@gmail.com>
2018-09-24 01:03:39 -0300

    changed php file to return 500 error if failed to save

--- write_data.php
+++ write_data.php
@@ -1,9 +1,14 @@
 <?php
-$post_data = json_decode(file_get_contents('php://input'), true); 
-// the directory "data" must be writable by the server
-$name = "../data/".$post_data['filename'].".csv"; 
-$data = $post_data['filedata'];
-// write the file to disk
-file_put_contents($name, $data);
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+	$post_data = json_decode(file_get_contents('php://input'), true);
+	// the directory "data" must be writable by the server
+	$name = "../data/".$post_data['filename'].".csv";
+	$data = $post_data['filedata'];
+	// write the file to disk
+	if (file_put_contents($name, $data)) {
+		http_response_code(200);
+	} else {
+		http_response_code(500);
+	}
+}
 ?>
-
