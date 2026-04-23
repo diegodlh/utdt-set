91d192b47a49df5a755921097248aed5a6995f51
Diego de la Hera <delahera@gmail.com>
2026-04-22 21:45:30 -0300

    Use HTML5 audio instead of WebAudio to try and fix known.mp3 sometimes not playing

--- jspsych-6.0.4/jspsych.js
+++ jspsych-6.0.4/jspsych.js
@@ -2370,7 +2370,10 @@ jsPsych.pluginAPI = (function() {
       loaded++;
       if(progress_bar){
         var percent_loaded = (loaded/total_n)*100;
-        jsPsych.getDisplayElement().querySelector('#jspsych-loading-progress-bar').style.width = percent_loaded+"%";
+        var bar = jsPsych.getDisplayElement().querySelector('#jspsych-loading-progress-bar');
+        if (bar) {
+          bar.style.width = percent_loaded+"%";
+        }
       }
     }
 
